c6edde2b07a13b91af7c36417fffa2e87be99eea
Hugo Hernani <hhernanni@gmail.com>
2021-03-07 17:22:29 -0300

    Initial commit (from base)

---
+++ lib/sneakers_23_web/router.ex
@@ -0,0 +1,29 @@
+#---
+# Excerpted from "Real-Time Phoenix",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/sbsockets for more book information.
+#---
+defmodule Sneakers23Web.Router do
+  use Sneakers23Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Sneakers23Web do
+    pipe_through :browser
+
+    get "/", ProductController, :index
+  end
+end
