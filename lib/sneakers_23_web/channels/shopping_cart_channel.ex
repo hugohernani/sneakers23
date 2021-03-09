b0d6b6d99803316bd37641659c6f80a54c7564a9
Hugo Hernani <hhernanni@gmail.com>
2021-03-09 19:57:16 -0300

    Handle PubSub subscription/unsubscription to deal with out of stock product

--- lib/sneakers_23_web/channels/shopping_cart_channel.ex
+++ lib/sneakers_23_web/channels/shopping_cart_channel.ex
@@ -10,11 +10,27 @@ defmodule Sneakers23Web.ShoppingCartChannel do
     cart = get_cart(params)
     socket = assign(socket, :cart, cart)
     send(self(), :send_cart)
+    enqueue_cart_subscriptions(cart)
 
     {:ok, socket}
   end
 
+  def handle_info({:subscribe, item_id}, socket) do
+    Phoenix.PubSub.subscribe(Sneakers23.PubSub, "item_out:#{item_id}")
+    {:noreply, socket}
+  end
+
+  def handle_info({:unsubscribe, item_id}, socket) do
+    Phoenix.PubSub.unsubscribe(Sneakers23.PubSub, "item_out:#{item_id}")
+    {:noreply, socket}
+  end
+
   def handle_info(:send_cart, socket = %{assigns: %{cart: cart}}) do
+    push(socket, "cart", cart_to_map(cart))
+    {:noreply, socket}
+  end
+
+  def handle_info({:item_out, _id}, socket = %{assigns: %{cart: cart}}) do
     push(socket, "cart", cart_to_map(cart))
     {:noreply, socket}
   end
@@ -23,6 +39,7 @@ defmodule Sneakers23Web.ShoppingCartChannel do
     "add_item", %{"item_id" => id}, socket = %{assigns: %{cart: cart}}) do
     case Checkout.add_item_to_cart(cart, String.to_integer(id)) do
       {:ok, new_cart} ->
+        send(self(), {:subscribe, id})
         broadcast_cart(new_cart, socket, added: [id])
         socket = assign(socket, :cart, new_cart)
         {:reply, {:ok, cart_to_map(new_cart)}, socket}
@@ -35,16 +52,18 @@ defmodule Sneakers23Web.ShoppingCartChannel do
     "remove_item", %{"item_id" => id}, socket = %{assigns: %{cart: cart}}) do
       case Checkout.remove_item_from_cart(cart, String.to_integer(id)) do
         {:ok, new_cart} ->
+          send(self(), {:unsubscribe, id})
           broadcast_cart(new_cart, socket, removed: [id])
           socket = assign(socket, :cart, new_cart)
           {:reply, {:ok, cart_to_map(new_cart)}, socket}
 
         {:error, :not_found} ->
           {:reply, {:error, %{error: "not_found"}}, socket}
-      end  
+      end
   end
 
   def handle_out("cart_updated", params, socket) do
+    modify_subscriptions(params)
     cart = get_cart(params)
     socket = assign(socket, :cart, cart)
     push(socket, "cart", cart_to_map(cart))
@@ -66,5 +85,17 @@ defmodule Sneakers23Web.ShoppingCartChannel do
     params
     |> Map.get("serialized", nil)
     |> Checkout.restore_cart()
+  end
+
+  defp enqueue_cart_subscriptions(cart) do
+    Checkout.cart_item_ids(cart)
+    |> Enum.each(fn id ->
+        send(self(), {:subscribe, id})
+    end)
+  end
+
+  defp modify_subscriptions(%{"added" => add, "removed" => remove}) do
+    Enum.each(add, & send(self(), {:subscribe, &1}))
+    Enum.each(remove, & send(self(), {:unsubscribe, &1}))
   end
 end
